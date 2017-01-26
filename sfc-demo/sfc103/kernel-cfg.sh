8ff66e6a47802e0f1090247846849d472dd58cb9
Phạm Hồng Thái <thaiph.hust@gmail.com>
2017-01-26 15:19:34 +0700

    Configure kernel before installing

---
+++ sfc-demo/sfc103/kernel-cfg.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+dpkg -l | grep linux-image | awk '{print $2}' > linux-image.lst
+input="$(pwd)/linux-image.lst"
+while IFS= read -r image 
+do
+  echo $image hold | sudo dpkg --set-selections 
+done < "$input"
+dpkg -l | grep "linux-image"
